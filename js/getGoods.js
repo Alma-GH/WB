39d789591943f4968da6936486e5275cf8065a95
Roman Kalimullin <cuni.21@yandex.ru>
2022-02-26 23:10:22 +0300

    get goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,11 +1,42 @@
 const getGoods = function () {
     const links = document.querySelectorAll(".navigation-link")
+    const viewAll = document.querySelector("a.more")
     
-    const getData = function(){
+
+    const renderGoods = function(goods){
+        const container = document.querySelector(".long-goods-list")
+        container.innerHTML = ""
+        goods.forEach(good=>{
+            const elem = document.createElement("div")
+            elem.classList.add("col-lg-3", "col-sm-6")
+            elem.innerHTML = `     
+            <div class="goods-card">
+                ${good.label?`<span class="label">${good.label}</span>`:""}
+                <img src="/db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+                <p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            `
+            container.appendChild(elem)
+        })
+    }
+    
+    const getData = function(cat, val){
         fetch("https://wildb-98f37-default-rtdb.europe-west1.firebasedatabase.app/db.json")
         .then(res=>res.json())
         .then(data=>{
-            localStorage.setItem("goods", JSON.stringify(data))
+            let array = cat?data.filter(good=>good[cat]===val):data
+
+            localStorage.setItem("goods", JSON.stringify(array))
+
+            if(window.location.pathname === "/goods.html"){
+               renderGoods(array)
+            } else {
+                window.location.href = "/goods.html"
+            }
         })
     }
 
@@ -14,12 +45,23 @@ const getGoods = function () {
     links.forEach(link => {
         link.addEventListener("click", e => {
             e.preventDefault()
-            console.log(e.target)
-            getData()
-            console.log(JSON.parse(localStorage.getItem("goods")))
+
+            const value = link.textContent
+            const category = link.dataset.field
+            getData(category, value)
         })
     })
 
+
+    viewAll?.addEventListener("click", e=>{
+        e.preventDefault()
+        const val = "All"
+        const cat = ""
+        getData(cat,val)
+    })
+
+    if(localStorage.getItem("goods") && window.location.pathname === "/goods.html") renderGoods(JSON.parse(localStorage.getItem("goods")))
+    
     
 }
 
--- js/search.js
+++ js/search.js
@@ -2,8 +2,44 @@ const search = function () {
     const input = document.querySelector(".search-block > input")
     const searchBtn = document.querySelector(".search-block > button")
 
+    const renderGoods = function(goods){
+        const container = document.querySelector(".long-goods-list")
+        container.innerHTML = ""
+        goods.forEach(good=>{
+            const elem = document.createElement("div")
+            elem.classList.add("col-lg-3", "col-sm-6")
+            elem.innerHTML = `     
+            <div class="goods-card">
+                ${good.label?`<span class="label">${good.label}</span>`:""}
+                <img src="/db/${good.img}" alt="image: Hoodie" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+                <p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            `
+            container.appendChild(elem)
+        })
+    }
+
+    const getData = function(search){
+        fetch("https://wildb-98f37-default-rtdb.europe-west1.firebasedatabase.app/db.json")
+        .then(res=>res.json())
+        .then(data=>{
+            let array = search?data.filter(good=>good.name.toLowerCase().includes(search.toLowerCase())):data
+            localStorage.setItem("goods", JSON.stringify(array))
+
+            if(window.location.pathname === "/goods.html"){
+               renderGoods(array)
+            } else {
+                window.location.href = "/goods.html"
+            }
+        })
+    }
+
     searchBtn.addEventListener("click", ()=>{
-        console.log(input.value)
+        getData(input.value)
     })
 }
 
